9179f2d4bd6cc2522fc95475dac04a90b43a59c1
RedWolfPunch <66182653+RedWolfPunch@users.noreply.github.com>
2023-07-28 21:08:25 +0000

    Update Install_Blender.sh
    Typo

--- Install_Blender.sh
+++ Install_Blender.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-curl https://download.blender.org/release/Blender3.5/blender-3.5.1-macos-x64.dmg" --output "/tmp/Blender.dmg"
+curl "https://download.blender.org/release/Blender3.5/blender-3.5.1-macos-x64.dmg" --output "/tmp/Blender.dmg"
 mkdir -p /tmp/Blender/
 hdiutil attach /tmp/Blender.dmg -nobrowse -noverify -mountpoint /tmp/Blender/
 rm -rf "/Applications/Blender.app"
